81ed2dfea21ee8fc67b8a3e6202d19f0d031167d
jhwshin <57696372+jhwshin@users.noreply.github.com>
2025-05-19 14:31:25 +1000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+sudo wget https://archive.kali.org/archive-keyring.gpg -O /usr/share/keyrings/kali-archive-keyring.gpg
+sudo apt update && sudo apt dist-upgrade && sudo apt upgrade && sudo apt autoremove
